8d4b7c136382025f19d39482831231c1792aa835
EnzoLEBRUN <44493282+EnzoLEBRUN@users.noreply.github.com>
2020-06-25 15:40:18 +0200

    Cookie

--- public/js/cookie.js
+++ public/js/cookie.js
@@ -29,8 +29,8 @@ function oft_cookieConsent() {
     $("body").append('<div id="oft_cookieConsent" style="position:fixed;bottom:0;right:0;width:100%;background-color:#343A40;padding:20px;color:#e0e0e0;">' +
         '<div style="display:inline-block;padding-right:20px;">\n' +
         'Nous utilisons les cookies afin de fournir les services et fonctionnalités proposés sur notre site et afin d’améliorer l’expérience de nos utilisateurs.\n' +
-        '<a class="text-warning" href="/templates/mentionsLegales/cgu.html.twig">Plus d\'information</a></div>' +
-        '<div class="text-center"><button id="oft_cookieConsent_valid" class="btn btn-warning" style="width:125px;margin-top: 20px;">Got it!</button></div>');
+        '<a class="text-warning" href="/conditions-generales-d-utilisation">Plus d\'information</a></div>' +
+        '<div class="text-center"><button id="oft_cookieConsent_valid" class="btn btn-warning" style="width:125px;margin-top: 20px;">J\'accepte !</button></div>');
     oft_checkCookie();
     $('#oft_cookieConsent_valid').on('click', function(){
         oft_setCookie("oft_cookieConsent");
